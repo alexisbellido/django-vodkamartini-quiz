3585a88360ec8d54252e30aec006ef978ff8cab6
Alexis Bellido <alexis@ventanazul.com>
2013-03-12 19:35:05 -0400

    use generic class base view FormView

--- vodkamartiniquiz/views.py
+++ vodkamartiniquiz/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.views.generic.base import View
 from django.views.generic import ListView
+from django.views.generic.edit import FormView
 from .models import Quiz
 from .forms import QuizForm
+
 
 class QuizHome(ListView):
     #model = Quiz
@@ -33,21 +36,32 @@ class QuizBasicDetail(View):
                       },
                       )
 
-class QuizCreate(View):
+#class QuizCreate(View):
+#    form_class = QuizForm
+#    initial = {'title': 'Quiz X', 'body': 'This is all about X'}
+#    template_name = 'vodkamartiniquiz/quiz_form.html'
+#
+#    def get(self, request, *args, **kwargs):
+#        form = self.form_class(initial=self.initial)
+#        return render(request, self.template_name, {'form': form})
+#
+#    def post(self, request, *args, **kwargs):
+#        form = self.form_class(request.POST)
+#        if form.is_valid():
+#            # process form cleaned_data
+#            quiz = form.save()
+#            print quiz
+#            #return HttpResponseRedirect(object.get_absolute_url())
+#            return HttpResponseRedirect(reverse('vodkamartiniquiz_quiz_home'))
+#        return render(request, self.template_name, {'form': form})
+
+class QuizCreate(FormView):
     form_class = QuizForm
     initial = {'title': 'Quiz X', 'body': 'This is all about X'}
     template_name = 'vodkamartiniquiz/quiz_form.html'
+    success_url = reverse_lazy('vodkamartiniquiz_quiz_home') # use reverse_lazy because URLconf is not processed yet
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            # process form cleaned_data
-            quiz = form.save()
-            print quiz
-            #return HttpResponseRedirect(object.get_absolute_url())
-            return HttpResponseRedirect(reverse('vodkamartiniquiz_quiz_home'))
-        return render(request, self.template_name, {'form': form})
+    def form_valid(self, form):
+        quiz = form.save()
+        print quiz
+        return super(QuizCreate, self).form_valid(form)
