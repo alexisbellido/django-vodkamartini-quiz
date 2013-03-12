eff17b7041f34d361a1f4091dd8d0b9fd0e67c33
Alexis Bellido <alexis@ventanazul.com>
2013-03-12 16:58:31 -0400

    add basic class based view for quiz detail

--- vodkamartiniquiz/urls.py
+++ vodkamartiniquiz/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
-from vodkamartiniquiz.views import QuizHome, QuizDetail
+from django.conf.urls.defaults import patterns, url
+from vodkamartiniquiz.views import QuizHome, QuizBasicDetail
 
 urlpatterns = patterns('vodkamartiniquiz.views',
-    #url(r'^$', 'quiz_index', {'page': 1}, name='vodkamartiniquiz_home'),
-    url(r'^$', QuizHome.as_view(), name='vodkamartiniquiz_home'),
-    url(r'^(?P<slug>[-\w]+)/$', QuizDetail.as_view(greeting='Hello'), name='vodkamartiniquiz_quiz_detail'),
+    url(r'^$', QuizHome.as_view(), name='vodkamartiniquiz_quiz_home'),
+    url(r'^(?P<slug>[-\w]+)/$', QuizBasicDetail.as_view(), name='vodkamartiniquiz_quiz_detail'),
     #url(r'^question/(?P<pk>\d+)/$', 'question_detail', name='vodkamartiniquiz_question_detail'),
     #url(r'^question/(?P<pk>\d+)/$', 'question_detail', name='vodkamartiniquiz_question_detail'),
-#    url(r'^page-(?P<page>\d+)/$', 'skeleton_index', name='vodkamartiniskeleton_index'),
 )
--- vodkamartiniquiz/views.py
+++ vodkamartiniquiz/views.py
@@ -1,45 +1,34 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response, get_object_or_404
-#from django.views.generic import TemplateView
 from django.views.generic.base import View
+from django.views.generic import ListView
+from vodkamartiniquiz.models import Quiz
 
-class QuizHome(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 
-                      'vodkamartiniquiz/quiz_list.html',
-                      {
-                        'object_list': ['a', 'b', 'c'],
-                      },
-                      )
+class QuizHome(ListView):
+    model = Quiz
+    #def get(self, request, *args, **kwargs):
+    #    return render(request, 
+    #                  'vodkamartiniquiz/quiz_list.html',
+    #                  {
+    #                    'object_list': ['a', 'b', 'c'],
+    #                  },
+    #                  )
 
-class QuizDetail(View):
+class QuizBasicDetail(View):
     greeting = 'Hola'
 
     def get(self, request, *args, **kwargs):
-        print "kwargs", kwargs
         if 'slug' in self.kwargs:
             slug = self.kwargs['slug']
+            object = Quiz.objects.get(slug=slug, status = Quiz.LIVE_STATUS)
 
         return render(request, 
-                      'vodkamartiniquiz/quiz_list.html',
+                      'vodkamartiniquiz/quiz_detail.html',
                       {
-                        'object_list': ['a', 'b', 'c'],
+                        'object': object,
                         'slug': slug,
                         'greeting': self.greeting,
                       },
                       )
 
-
-def quiz_index(request, page=1):
-    """
-    Explain the purpose of this view.
-    """
-
-    # do something
-    output = "A test for quiz app"
-    return HttpResponse(output)
-
-def question_detail(request, pk):
-    output = "Question %s for quiz" % pk
-    return HttpResponse(output)
