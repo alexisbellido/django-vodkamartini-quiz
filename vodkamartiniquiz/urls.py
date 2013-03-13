a762f3d26848d5e692dd393f1e917f3dae5f5cc0
Alexis Bellido <alexis@ventanazul.com>
2013-03-13 11:07:09 -0400

    prepare quiz application to use sass

--- vodkamartiniquiz/urls.py
+++ vodkamartiniquiz/urls.py
@@ -5,7 +5,7 @@ from .views import QuizCreate
 urlpatterns = patterns('vodkamartiniquiz.views',
     url(r'^$', QuizHome.as_view(), name='vodkamartiniquiz_quiz_home'),
     url(r'^add/$', QuizCreate.as_view(), name='vodkamartiniquiz_quiz_create'),
-    #url(r'^(?P<slug>[-\w]+)/$', QuizBasicDetail.as_view(), name='vodkamartiniquiz_quiz_detail'),
+    url(r'^(?P<slug>[-\w]+)/$', QuizBasicDetail.as_view(), name='vodkamartiniquiz_quiz_detail'),
     #url(r'^question/(?P<pk>\d+)/$', 'question_detail', name='vodkamartiniquiz_question_detail'),
     #url(r'^question/(?P<pk>\d+)/$', 'question_detail', name='vodkamartiniquiz_question_detail'),
 )
